9f53554055a85466dc5527decd8549f8baebc0b4
dbm39 <dbm39@gcloud.ua.es>
2025-05-01 11:18:18 +0200

    ETL de Fecha hecho

---
+++ ETLs/Fecha.py
@@ -0,0 +1,56 @@
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql import functions, Window, when
+from datetime import datetime
+
+from pyspark.sql.functions import col, expr, to_date, year, month, dayofmonth, concat_ws, when
+
+#mysql-connector-j-9.3.0.jar
+
+# Generamos la fecha inicial y final de la NBA (segun los datos usados)
+fecha_inicial = "1946-01-01"
+fecha_final = "2019-12-31"
+
+# Calculamos el numero total de dias
+inicio = datetime.strptime(fecha_inicial, "%Y-%m-%d")
+fin = datetime.strptime(fecha_final, "%Y-%m-%d")
+dias = (fin - inicio).days + 1
+
+# Creamos un DataFrame con un rango de días
+df = pyspark.range(0, dias).withColumn(
+    "fecha", expr(f"date_add('{fecha_inicial}', id)")
+)
+
+# Obtenemos los datos requeridos del dataframe creado
+df = df.select(
+    col("fecha"),
+    year("fecha").alias("ano"),
+    month("fecha").alias("mes"),
+    dayofmonth("fecha").alias("dia")
+)
+
+# Ordenamos las fechas antes de añadir la id
+df = df.orderBy("fecha", ascending=True)
+
+# Generamos la id de cada fecha
+df = df.withColumn("idFecha", functions.monotonically_increasing_id())
+
+# Calculamos la temporada segun la fecha especifica
+df = df.withColumn(
+    "ano_inicio_temporada",
+    when(col("mes") >= 10, col("ano")).otherwise(col("ano") - 1)
+)
+
+df = df.withColumn(
+    "temporada",
+    concat_ws("-", col("ano_inicio_temporada"), (col("ano_inicio_temporada") + 1))
+)
+
+# Eliminar columnas innecesarias
+df = df.drop("fecha")
+
+# Mostramos la tabla final
+df.show()
+
+# Almacenamos el resultado en Hive
+df.write.mode("overwrite").insertInto("mydb.fecha")
