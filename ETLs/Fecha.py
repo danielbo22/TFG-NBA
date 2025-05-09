1203d1fbdc137b2ab82ab660bd5499dee96d4d33
dbm39 <dbm39@gcloud.ua.es>
2025-05-09 13:06:43 +0200

    ETL de fecha funcional y correcto

--- ETLs/Fecha.py
+++ ETLs/Fecha.py
@@ -1,12 +1,20 @@
 import pyspark
 from pyspark.sql import SparkSession
-from pyspark.sql import functions, Window, when
+from pyspark.sql import functions, Window
 from datetime import datetime
 
 from pyspark.sql.functions import col, expr, to_date, year, month, dayofmonth, concat_ws, when
+from pyspark.sql.types import IntegerType, StringType
 
 #mysql-connector-j-9.3.0.jar
 
+# Crear la sesión Spark
+spark = SparkSession.builder \
+                    .config("spark.sql.warehouse.dir", "hdfs://localhost:9000/user/hive/warehouse") \
+                    .appName("TFG NBA") \
+                    .enableHiveSupport() \
+                    .getOrCreate()
+ 
 # Generamos la fecha inicial y final de la NBA (segun los datos usados)
 fecha_inicial = "1946-01-01"
 fecha_final = "2019-12-31"
@@ -17,16 +25,16 @@ fin = datetime.strptime(fecha_final, "%Y-%m-%d")
 dias = (fin - inicio).days + 1
 
 # Creamos un DataFrame con un rango de días
-df = pyspark.range(0, dias).withColumn(
-    "fecha", expr(f"date_add('{fecha_inicial}', id)")
+df = spark.range(0, dias).withColumn(
+    "fecha", expr(f"date_add('{fecha_inicial}', CAST(id AS INT))")
 )
 
 # Obtenemos los datos requeridos del dataframe creado
 df = df.select(
     col("fecha"),
-    year("fecha").alias("ano"),
-    month("fecha").alias("mes"),
-    dayofmonth("fecha").alias("dia")
+    year("fecha").cast(IntegerType()).alias("ano"),
+    month("fecha").cast(IntegerType()).alias("mes"),
+    dayofmonth("fecha").cast(IntegerType()).alias("dia")
 )
 
 # Ordenamos las fechas antes de añadir la id
@@ -47,10 +55,19 @@ df = df.withColumn(
 )
 
 # Eliminar columnas innecesarias
-df = df.drop("fecha")
+df = df.drop("fecha", "ano_inicio_temporada")
 
 # Mostramos la tabla final
 df.show()
 
+# Cast explícito de todas las columnas según los tipos esperados en Hive
+df = df.select(
+    col("idFecha").cast(IntegerType()).alias("idFecha"),
+    col("dia").cast(IntegerType()).alias("dia"),
+    col("mes").cast(IntegerType()).alias("mes"),
+    col("ano").cast(IntegerType()).alias("ano"),
+    col("temporada").cast(StringType()).alias("temporada")
+)
+
 # Almacenamos el resultado en Hive
-df.write.mode("overwrite").insertInto("mydb.fecha")
+df.write.mode("overwrite").saveAsTable("mydb.fecha")
